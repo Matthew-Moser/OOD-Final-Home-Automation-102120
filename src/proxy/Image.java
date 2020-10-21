560410dd867b683ed72e8ba73412a83c4885d25a
Matthew-Moser <mattmoserp8@gmail.com>
2020-10-21 18:36:08 -0500

    Proxy

---
+++ src/proxy/Image.java
@@ -0,0 +1,5 @@
+package proxy;
+//proxy design for a list of fun facts that are stored in the cloud and accessed via wifi that the home automation utilizes
+public interface Image {
+	void displayFunFact();
+}
