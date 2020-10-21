3d3e5f35d570e4cc17955b99e849eb8baceb5461
Matthew-Moser <mattmoserp8@gmail.com>
2020-10-21 18:36:14 -0500

    State Pattern

---
+++ src/state/AutoLock.java
@@ -0,0 +1,5 @@
+package state;
+
+public interface AutoLock {
+	public void activateLock(Context context);
+}
---
+++ src/state/Context.java
@@ -0,0 +1,15 @@
+package state;
+
+public class Context {
+	private AutoLock autoLock;
+	
+	public Context() {
+		autoLock = null;
+	}
+	public void setAutoLock (AutoLock autoLock) {
+		this.autoLock = autoLock;
+	}
+	public AutoLock getAutoLock() {
+		return autoLock;
+	}
+}
