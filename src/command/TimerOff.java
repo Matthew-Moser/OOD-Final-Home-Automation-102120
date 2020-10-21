dd9d67fd5f02f3586b4d64c01b2dffc0a65d04a1
Matthew-Moser <mattmoserp8@gmail.com>
2020-10-21 18:35:31 -0500

    Command Pattern

---
+++ src/command/TimerOff.java
@@ -0,0 +1,14 @@
+package command;
+
+public class TimerOff implements Command {
+	Timer timer;
+	public TimerOff (Timer timer) {
+		this.timer=timer;
+	}
+	@Override
+	public void execute() {
+		timer.stopClock();
+		
+	}
+
+}
---
+++ src/command/TimerOn.java
@@ -0,0 +1,12 @@
+package command;
+
+public class TimerOn implements  Command{
+	Timer timer;
+	public TimerOn (Timer timer) {
+	this.timer=timer;
+	}
+	@Override
+	public void execute() {
+		timer.startClock();		
+	}
+}
